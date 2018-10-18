ba7277d04f710e3e917358b1c0d86179127eee4e
Grigori Draci <grigori@smartsoftdev.eu>
2018-10-18 14:57:05 +0300

    sync page download

---
+++ src/sync/page_down.py
@@ -0,0 +1,13 @@
+import requests
+from .loggin_util import logger
+
+
+def download_page(url):
+    logger.info("downloading the page")
+    page = request.get(url)
+    logger.debug(f"page status : {page.status_code}")
+    if page.status_code == 200:
+        logger.info("page downloaded with succes")
+    else:
+        logger.warning("page didn't downloaded")
+
